e4055c2bd03a53c35bb3d1e35338a48d62a2bfed
Luthaf <luthaf@luthaf.fr>
2022-06-30 15:21:44 +0200

    Make rascaline use pre-build equistore wheels

---
+++ rascaline/change-equistore-dependency.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eux
+
+rm -rf tmp
+
+mkdir tmp
+cd tmp
+tar xf ../rascaline-*.tar.gz
+RASCALINE_DIR=`ls`
+sed -i "" -e "s|equistore @ https://github.com/lab-cosmo/equistore/archive/513889.zip|equistore|" ${RASCALINE_DIR}/setup.cfg
+tar czf ${RASCALINE_DIR}.tar.gz ${RASCALINE_DIR}
+
+rm -rf ${RASCALINE_DIR}
+
+for path in `ls ../*.whl`; do
+    wheel=`basename $path`
+    mkdir $wheel-dir
+    cd $wheel-dir
+    unzip ../$path
+
+    sed -i "" -e "s|equistore @ https://github.com/lab-cosmo/equistore/archive/513889.zip|equistore|" *.dist-info/METADATA
+
+    zip -r ../$wheel *
+    cd ..
+
+    rm -rf $wheel-dir
+done
+
+cd ..
+
+mv tmp/* .
+rm -rf tmp
